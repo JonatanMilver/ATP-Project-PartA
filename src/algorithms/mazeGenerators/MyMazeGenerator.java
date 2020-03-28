9d69355c8c7869b28b2dcbcf9476bd79743aa7b4
Guyzaidman <guyzaidm@gmail.com>
2020-03-28 20:12:27 +0300

    added random_start_goal_position function to myMazeGen

--- src/algorithms/mazeGenerators/MyMazeGenerator.java
+++ src/algorithms/mazeGenerators/MyMazeGenerator.java
@@ -10,7 +10,9 @@ public class MyMazeGenerator extends AMazeGenerator {
 
     @Override
     public Maze generate(int rows, int columns) {
-        ArrayList<Position> startGoal = randomStartGoal(rows, columns);
+
+//        ArrayList<Position> startGoal = randomStartGoal(rows, columns);
+        ArrayList<Position> startGoal = totalyRandomStartGoal(rows, columns);
         //generating new maze with start position in index (0,0) and goal position in index (r-1,c-1)
         Maze mymaze = new Maze(rows,columns,startGoal.get(0),startGoal.get(1));
 
@@ -23,6 +25,7 @@ public class MyMazeGenerator extends AMazeGenerator {
 
         return mymaze;
     }
+
 
     private void DFS_algorithm(Stack<Position> positionStack, Maze mymaze){
 
@@ -55,6 +58,227 @@ public class MyMazeGenerator extends AMazeGenerator {
                 positionStack.push(chosenNeighbour);
             }
         }
+    }
+
+    private ArrayList<Position> totalyRandomStartGoal(int rows, int columns) {
+        ArrayList<Position> startNgoal = new ArrayList<>();
+        Random randgen = new Random();
+
+        Position start;
+        Position end;
+        int start_edge = randgen.nextInt(4); // 0 - up , 1 - right , 2 - down , 3 - left
+        int start_pos, end_edge , end_pos;
+        if (start_edge == 0 || start_edge == 2){
+            start_pos = randgen.nextInt(columns);
+
+            // start position in (0,?)
+            if (start_edge == 0){
+
+                //start position in (0,0)
+                if (start_pos == 0){
+                    start = new Position(0,0);
+                    end_edge = randgen.nextInt(2); // 0 - right , 1 - down
+                    if (end_edge == 0){
+                        end_pos = randgen.nextInt(rows-1)+1;
+                        end = new Position(end_pos,columns-1);
+                    }
+                    else { //end_edge == 1
+                        end_pos = randgen.nextInt(columns-1)+1;
+                        end = new Position(rows-1 , end_pos);
+                    }
+                }
+
+                //start position in (0,columns-1)
+                else if (start_pos == columns-1) {
+                    start = new Position(0,columns-1);
+                    end_edge = randgen.nextInt(2); // 0 - left , 1 - down
+                    if (end_edge == 0){
+                        end_pos = randgen.nextInt(rows-1)+1;
+                        end = new Position(end_pos,0);
+                    }
+                    else{ //end_edge == 1
+                        end_pos = randgen.nextInt(columns-1);
+                        end = new Position(rows-1 , end_pos);
+                    }
+                }
+
+                else{
+                    start = new Position(0,start_pos);
+                    end_edge = randgen.nextInt(3); // 0 - left , 1 - down , 2 - right
+                    if (end_edge == 0){
+                        end_pos = randgen.nextInt(rows-1)+1;
+                        end = new Position(end_pos,0);
+                    }
+                    else if (end_edge == 2){
+                        end_pos = randgen.nextInt(rows-1)+1;
+                        end = new Position(end_pos,columns-1);
+                    }
+                    else{ // end_pos == 1
+                        end_pos = randgen.nextInt(columns);
+                        end = new Position(rows-1 , end_pos);
+                    }
+                }
+            }
+
+            // start position in (rows-1,?)
+            else{ //start_edge == 2
+
+                //start position in (rows-1,0)
+                if (start_pos == 0){
+                    start = new Position(rows-1,0);
+                    end_edge = randgen.nextInt(2); // 0 - right , 1 - up
+                    if (end_edge == 0){
+                        end_pos = randgen.nextInt(rows-1);
+                        end = new Position(end_pos,columns-1);
+                    }
+                    else { //end_edge == 1
+                        end_pos = randgen.nextInt(columns-1)+1;
+                        end = new Position(rows-1 , end_pos);
+                    }
+                }
+
+
+                //start position in (rows-1,columns-1)
+                else if (start_pos == columns-1){
+                    start = new Position(rows-1,columns-1);
+                    end_edge = randgen.nextInt(2); // 0 - left , 1 - up
+                    if (end_edge == 0){
+                        end_pos = randgen.nextInt(rows-1);
+                        end = new Position(end_pos,0);
+                    }
+                    else{ //end_edge == 1
+                        end_pos = randgen.nextInt(columns-1);
+                        end = new Position(0 , end_pos);
+                    }
+                }
+
+                else{
+                    start = new Position(rows-1,start_pos);
+                    end_edge = randgen.nextInt(3); // 0 - left , 1 - up , 2 - right
+                    if (end_edge == 0){
+                        end_pos = randgen.nextInt(rows-1);
+                        end = new Position(end_pos,0);
+                    }
+                    else if (end_edge == 2){
+                        end_pos = randgen.nextInt(rows-1);
+                        end = new Position(end_pos,columns-1);
+                    }
+                    else{ // end_pos == 1
+                        end_pos = randgen.nextInt(columns);
+                        end = new Position(0 , end_pos);
+                    }
+                }
+            }
+
+        }
+
+        else{ //start_edge == 1 || start_edge == 3
+            start_pos = randgen.nextInt(rows);
+
+            // start position in right-most column (?,columns-1)
+            if (start_edge == 1){
+
+                //start position in (0,columns-1)
+                if (start_pos==0){
+                    start = new Position(start_pos,columns-1);
+                    end_edge = randgen.nextInt(2); // 0 - left , 1 - down
+                    if (end_edge == 0){
+                        end_pos = randgen.nextInt(rows-1)+1;
+                        end = new Position(end_pos,0);
+                    }
+                    else {
+                        end_pos = randgen.nextInt(columns-1)+1;
+                        end = new Position(rows-1,end_pos);
+                    }
+                }
+
+                //start position in (rows-1,columns-1)
+                else if (start_pos == rows-1){
+                    start = new Position(start_pos,columns-1);
+                    end_edge = randgen.nextInt(2); // 0 - left , 1 - up
+                    if (end_edge == 0){
+                        end_pos = randgen.nextInt(rows-1);
+                        end = new Position(end_pos,0);
+                    }
+                    else{
+                        end_pos = randgen.nextInt(columns-1);
+                        end = new Position(0 , end_pos);
+                    }
+                }
+
+                else{
+                    start = new Position(start_pos,columns-1);
+                    end_edge = randgen.nextInt(3); // 0 - left , 1 - up , 2 - down
+                    if (end_edge == 0){
+                        end_pos = randgen.nextInt(rows);
+                        end = new Position(end_pos,0);
+                    }
+                    else if (end_edge == 1){
+                        end_pos = randgen.nextInt(columns-1);
+                        end = new Position(0,end_pos);
+                    }
+                    else{
+                        end_pos = randgen.nextInt(columns-1);
+                        end = new Position(rows-1,end_pos);
+                    }
+                }
+            }
+
+            // start position in left-most column (?,0)
+            else{ //start_edge == 3
+
+                //start position in (0,0)
+                if (start_pos == 0){
+                    start = new Position(0,0);
+                    end_edge = randgen.nextInt(2); // 0 - right , 1 - down
+                    if (end_edge == 0){
+                        end_pos = randgen.nextInt(rows-1)+1;
+                        end = new Position(end_pos,columns-1);
+                    }
+                    else {
+                        end_pos = randgen.nextInt(columns-1)+1;
+                        end = new Position(rows-1,end_pos);
+                    }
+                }
+
+                //start position in (rows-1,0)
+                else if (start_pos == rows-1){
+                    start = new Position(start_pos,0);
+                    end_edge = randgen.nextInt(2); // 0 - right , 1 - up
+                    if (end_edge == 0){
+                        end_pos = randgen.nextInt(rows-1)+1;
+                        end = new Position(end_pos,columns-1);
+                    }
+                    else{
+                        end_pos = randgen.nextInt(columns-1)+1;
+                        end = new Position(0 , end_pos);
+                    }
+                }
+
+                else{
+                    start = new Position(start_pos , 0);
+                    end_edge = randgen.nextInt(3); // 0 - up , 1 - right , 2 - down
+                    if (end_edge == 0){
+                        end_pos = randgen.nextInt(columns-1)+1;
+                        end = new Position(0 , end_pos);
+                    }
+                    else if (end_edge == 2){
+                        end_pos = randgen.nextInt(columns-1)+1;
+                        end = new Position(rows-1,end_pos);
+                    }
+                    else{
+                        end_pos = randgen.nextInt(rows);
+                        end = new Position(end_pos,columns-1);
+                    }
+                }
+
+            }
+
+        }
+
+        startNgoal.add(start);
+        startNgoal.add(end);
+        return startNgoal;
     }
 
     private ArrayList<Position> randomStartGoal(int rows, int columns){
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,17 +9,15 @@ public class Main {
 
     public static void main(String[] args) {
         AMazeGenerator s = new MyMazeGenerator();
-        Maze mazz = s.generate(5,10);
+        Maze mazz = s.generate(50,100);
         mazz.print();
         ISearchable a = new SearchableMaze(mazz);
-        AState k = a.getStartState().getDown_state();
-        //ISearchingAlgorithm search = new BreadthFirstSearch("BFS");
-        ISearchingAlgorithm search = new DepthFirstSearch("DFS");
+        ISearchingAlgorithm search = new BreadthFirstSearch("BFS");
+//        ISearchingAlgorithm search = new DepthFirstSearch("DFS");
         Solution sol = search.solve(a);
-        sol.print();
-        System.out.println(search.getNumberOfNodesEvaluated());
-        System.out.println(mazz.getStartPosition().getMovable_neighbours());
+        System.out.println(mazz.getStartPosition());
         System.out.println(mazz.getGoalPosition());
+        sol.print();
 
 
 //        System.out.println(s.measureAlgorithmTimeMillis(1000,1000));
